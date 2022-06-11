a09b285139ff0e0cedc0d769da32ebb3006955a6
Anna Vilnid <anna.vilnid@gmail.com>
2022-06-11 21:30:32 +0200

    loader fixed

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -2,17 +2,7 @@ import React from 'react';
 
 function PopupWithForm({ name, title, isOpen, onClose, formName, btnName="Сохранить", children, onSubmit, switchLoader, buttonDisabled}) {
 
-  console.log(switchLoader)
-  console.log(buttonDisabled)
   const handleButtonName = () => switchLoader ? "Сохранение..." : btnName
-  // const handleDisabled = () => {
-  // if (switchLoader === true || buttonDisabled === true ) {
-  //     console.log(true)
-  //   } else {
-  //     console.log(false)
-  //     return false 
-  //   }
-//}
 
 // Разметка 
   return (
@@ -22,7 +12,7 @@ function PopupWithForm({ name, title, isOpen, onClose, formName, btnName="Сох
         <h2 className="popup__title">{title}</h2>
         <form className={`popup__form popup__form_action_${name}`} name={formName} noValidate onSubmit={onSubmit}>
           {children}
-          <button className={`popup__save-button ${buttonDisabled && 'popup__save-button_disabled'}`} type="submit" disabled={buttonDisabled}>{handleButtonName()}</button>
+          <button className={`popup__save-button ${buttonDisabled && 'popup__save-button_disabled'}`} type="submit" disabled={buttonDisabled || switchLoader}>{handleButtonName()}</button>
        </form>
       </div>
     </div>
