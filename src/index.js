b0294b42ad70b5b071b2fd89cd3f102ef7987ac8
Anna Vilnid <anna.vilnid@gmail.com>
2022-06-25 18:23:49 +0200

    gh-pages 4

--- src/index.js
+++ src/index.js
@@ -5,11 +5,10 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import {HashRouter} from "react-router-dom";
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename={"/react-mesto-auth"}>
+    <HashRouter>
       <App/>
     </HashRouter>
   </React.StrictMode>
