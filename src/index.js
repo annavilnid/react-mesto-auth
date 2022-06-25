f49078337b986c4b60d35a654c7c05aa4a1798fd
Anna Vilnid <anna.vilnid@gmail.com>
2022-06-25 18:09:56 +0200

    gh-pages

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={"/react-mesto-auth"}>
+    <HashRouter basename={"/react-mesto-auth"}>
       <App/>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
